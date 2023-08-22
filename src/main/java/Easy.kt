8e92f1c74b54ca6687667cc45fe642fe3cb1bafb
@kaman4 <kaman@tekion.com>
2023-08-22 20:43:20 +0530

    Info: Solution -> gcdStrings() Problem -> string Optimised -> true

--- src/main/java/Easy.kt
+++ src/main/java/Easy.kt
@@ -12,13 +12,31 @@ class Easy {
         finalString += mainString
         return finalString
     }
+
+    fun gcdStrings(str1: String, str2: String): String{
+        if (str1+str2 != str2+str1){
+            return ""
+        }
+        if (str1 == str2){
+            return str1
+        }
+        if (str1.length > str2.length){
+            return gcdStrings(str2, str1)
+        }
+        if(str2.startsWith(str2)){
+            return gcdStrings(str1, str2.substring(str1.length))
+        }
+        return ""
+    }
 }
 
 fun main() {
     val w1 = "ab"
     val w2 = "pqrs"
+    val str1 = "LEET"
+    val str2 = "CODE"
     val easy = Easy()
 
-    println(easy.alterMerge(w1, w2))
+    println(easy.gcdStrings(str1, str2))
 }
 
