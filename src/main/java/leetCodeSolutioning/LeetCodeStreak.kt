d43c33885b12c3eed0ff59e2b46df0c059218814
@kaman4 <kaman@tekion.com>
2023-10-02 18:04:01 +0530

    Info: Solution -> winnerOfGame Algorithm -> Problem -> Optimised -> true Structuring -> Updated and organised

--- src/main/java/leetCodeSolutioning/LeetCodeStreak.kt
+++ src/main/java/leetCodeSolutioning/LeetCodeStreak.kt
@@ -1,5 +1,4 @@
 package leetCodeSolutioning
-
 class LeetCodeStreak {
 
     fun reverseWords(s: String): String {
@@ -18,11 +17,26 @@ class LeetCodeStreak {
         return s.split(" ".toRegex()).joinToString(" ") {it.reversed()}
     }
 
+    fun winnerOfGame(colors: String): Boolean {
+        var countA = 0
+        var countB = 0
+
+        for (i in 1 until colors.length - 1) {
+            if (colors[i - 1] == 'A' && colors[i] == 'A' && colors[i + 1] == 'A') {
+                countA++
+            } else if (colors[i - 1] == 'B' && colors[i] == 'B' && colors[i + 1] == 'B') {
+                countB++
+            }
+        }
+
+        return countA > countB
+    }
+
 }
 
 
 fun main() {
     val leetCodeStreak = LeetCodeStreak()
-    var testString = "This is a test string"
-    println(leetCodeStreak.reverseWords(testString))
+    var testString = "ABBBBBBBAAA"
+    println(leetCodeStreak.winnerOfGame(testString))
 }
